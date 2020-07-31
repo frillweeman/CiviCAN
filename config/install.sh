6e9e8e256d671f2cfd36099b931d09e913a724aa
Will Freeman <hohosanta@me.com>
2020-07-31 03:04:31 +0100

    update config script

--- config/install.sh
+++ config/install.sh
@@ -2,6 +2,27 @@
 
 # run as ROOT (or with sudo)
 
+# prompt user for new hostname
+read -p "Would you like to change the hostname? [Y/n] " yn
+case $yn in
+    [Yy]* )
+	read -p "New hostname: " NEWHOST
+        ;;
+esac
+
+# Message of the Day (displays at login)
+cp motd /etc/
+
+# basic setup
+raspi-config nonint do_expand_rootfs     # expand FS
+raspi-config nonint do_boot_behaviour B2 # autologin command line
+raspi-config nonint do_boot_wait 1       # don't wait for network on boot
+raspi-config nonint do_ssh 0             # enable SSH
+
+
+# apt dependencies
+apt install -y git pipenv
+
 # copy systemd service units
 echo "copying systemd service units"
 cp gpio-shutdown.service vehicle.service /etc/systemd/system/
@@ -20,4 +41,12 @@ echo "reloading udev rules"
 udevadm control --reload-rules
 udevadm trigger
 
-echo "Done"
+# change hostname
+if [[ $NEWHOST ]]; then
+	raspi-config nonint do_hostname $NEWHOST
+fi
+
+echo "Done. Press any key to restart."
+
+read
+reboot
